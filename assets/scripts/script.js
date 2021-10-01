1aa789fac6f14d4bfef453372b70209440274aba
Supratim Nag <supratimnag1997@gmail.com>
2021-10-01 13:04:38 +0530

    fixed details card border overlap

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -9,6 +9,7 @@ const containerElem = document.querySelector(".container");
 const mainPageElem = document.querySelector(".main-page");
 const detailsPageElem = document.querySelector(".details-page");
 const detailsPageHeaderElem = document.querySelector(".details-page .header-title h2");
+const detailsPageMainElem = document.querySelector(".details-page main");
 const detailsPageTaskCardElem = document.querySelector(".details-page .task-list");
 const taskListsElem = document.querySelector(".task-lists");
 const fallbackMsgELem = document.querySelector(".fallback");
@@ -167,8 +168,8 @@ const renderTaskDetails = (tasklistId) => {
   const taskLists = operations.getTaskLists();
   const tasklist = taskLists.find((taskList) => taskList.id === tasklistId);
   detailsPageHeaderElem.textContent = tasklist.title;
-  detailsPageTaskCardElem.innerHTML = "";
-  detailsPageTaskCardElem.insertAdjacentHTML("afterbegin", singleTaskCardHTML(tasklist, false));
+  detailsPageMainElem.innerHTML = "";
+  detailsPageMainElem.insertAdjacentHTML("afterbegin", singleTaskCardHTML(tasklist, false));
 };
 const toggleScreenMobile = () => {
   mainPageElem.classList.toggle(hidden);
